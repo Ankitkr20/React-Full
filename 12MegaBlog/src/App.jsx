9ae73035641678e513a4dd315119a10b85fcd7f9
Ankitkr20 <18krankit@gmail.com>
2025-07-18 19:19:29 +0530

    new

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,38 @@
+import React,{useState, useEffect} from 'react'
+import { useDispatch }  from 'react-redux'
+import './App.css'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Outlet} from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dipatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then( (userData) => {
+      if(userData){
+        dipatch(login({userData}))
+      }else{
+        dispatchEvent(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  return !loading?(
+    <div className='min-h-screen flex flex-wrpa content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
+}
+
+export default App
